a40c35f5f3699b0fed2957aa3debc9a5ade961ef
Marie-EveGauthier <gauthier.marie.eve@gmail.com>
2024-03-19 12:00:53 -0400

    Implement a back to home button in coco page

--- src/components/EasterLink.tsx
+++ src/components/EasterLink.tsx
@@ -1,13 +1,13 @@
 import styled from "styled-components";
 import { Link } from "react-router-dom";
 
-const StyledLink = styled(Link)`
+const StyledLink = styled(Link)<{ fontSize?: string }>`
   display: flex;
   gap: 4px;
   align-items: center;
   text-decoration: none;
   font-weight: 600;
-  font-size: 0.8rem;
+  font-size: ${({ fontSize }) => (fontSize ? fontSize : "0.8rem")};
 
   color: var(--color-white);
   background-color: var(--color-yellow);
@@ -17,8 +17,12 @@ const StyledLink = styled(Link)`
   margin: 5px;
 `;
 
-const EasterLink = ({ to, children }) => {
-  return <StyledLink to={to}>{children}</StyledLink>;
+const EasterLink = ({ to, children, fontSize }) => {
+  return (
+    <StyledLink to={to} fontSize={fontSize}>
+      {children}
+    </StyledLink>
+  );
 };
 
 export default EasterLink;
--- src/routes/coco.tsx
+++ src/routes/coco.tsx
@@ -1,3 +1,28 @@
-export default function Coco() {
-  return <div>Je suis un coco</div>;
-}
+import styled from "styled-components";
+import { FaArrowLeftLong } from "react-icons/fa6";
+
+import EasterLink from "../components/EasterLink";
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const WrapperLink = styled.nav`
+  width: fit-content;
+  margin-top: 2rem;
+`;
+
+const Coco = () => {
+  return (
+    <Wrapper>
+      <WrapperLink>
+        <EasterLink to="/" fontSize="1.2rem">
+          <FaArrowLeftLong /> Retour aux cocos
+        </EasterLink>
+      </WrapperLink>
+    </Wrapper>
+  );
+};
+
+export default Coco;
