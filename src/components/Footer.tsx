df469005a5f159139d2e2027e7a06b0870724b09
Marie-EveGauthier <gauthier.marie.eve@gmail.com>
2024-03-24 18:43:52 -0400

    The footer is fixed to the bottom

---
+++ src/components/Footer.tsx
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+import { GoHeartFill } from "react-icons/go";
+
+const Wrapper = styled.footer`
+  margin-top: auto;
+  padding: 3rem 0;
+  color: var(--color-green-dark);
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  gap: 4px;
+`;
+
+const Footer = () => {
+  return (
+    <Wrapper>
+      Fait avec <GoHeartFill color="var(--color-pink-dark)" /> et un goût de
+      chocolat 🐥
+    </Wrapper>
+  );
+};
+
+export default Footer;
--- src/routes/Layout.tsx
+++ src/routes/Layout.tsx
@@ -1,19 +1,14 @@
 import styled from "styled-components";
 import { Outlet } from "react-router-dom";
+
 import MaxWidthWrapper from "../components/MaxWidthWrapper";
 import Header from "../components/Header";
+import Footer from "../components/Footer";
 
 const Wrapper = styled.div`
   background-color: var(--color-green-light);
   height: 100%;
   min-height: 100vh;
-`;
-
-const Footer = styled.footer`
-  flex-shrink: 0;
-  margin: 1rem;
-  padding: 64px;
-  color: var(--color-green-dark);
 `;
 
 export const Layout = () => {
@@ -22,9 +17,7 @@ export const Layout = () => {
       <MaxWidthWrapper>
         <Header />
         <Outlet />
-        <Footer>
-          <p>Fait avec amour</p>
-        </Footer>
+        <Footer />
       </MaxWidthWrapper>
     </Wrapper>
   );
